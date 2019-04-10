1379f8825038083df9b1f39b4a80d6bd925c934b
Ponnusamy K <ponnusamy@francium.tech>
2019-04-10 17:55:58 +0530

    Extra Exercise 2 Leap year find  exercise

---
+++ leap/leap.go
@@ -0,0 +1,14 @@
+package leap
+
+// IsLeapYear should have a comment documenting it.
+func IsLeapYear(year int) bool {
+	if year%400 == 0 {
+		return true
+	} else if year%100 == 0 {
+		return false
+	} else if year%4 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
